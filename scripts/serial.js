87b4a8218a94bdf0e7225ffa835aa2242b86ae43
术生何 <urfdvw@gmail.com>
2023-02-06 21:50:09 -0500

    matcher and brancher

--- scripts/serial.js
+++ scripts/serial.js
@@ -1,3 +1,71 @@
+class Matcher {
+    constructor (target) {
+        this.target = target;
+        this.segment = "";
+    }
+    push (segment) {
+        let result = [];
+        segment = this.segment + segment;
+        // see if the tail contains partial target
+        for (let i = segment.length - this.target.length; i < segment.length; i++) {
+            if (i < 0) {
+                continue;
+            }
+            let tail = segment.slice(i);
+            if (tail == this.target.slice(0, tail.length)) {
+                this.segment = tail;
+                segment = segment.slice(0, segment.length - tail.length);
+                break;
+            } else {
+                this.segment = "";
+            }
+        }
+        let parts = segment.split(this.target);
+        // first output
+        let first_part = parts.shift();
+        if (first_part.length > 0) {
+            result.push([first_part, false]);
+        }
+        // the rest
+        for (const p of parts) {
+            result.push([this.target, true]);
+            result.push([p, false]);
+        }
+        return result;
+    }
+}
+
+class Brancher {
+    constructor (begin_str, end_str) {
+        this.begin_matcher = new Matcher(begin_str);
+        this.end_matcher = new Matcher(end_str);
+        this.mood = false;
+        this.matcher = this.begin_matcher;
+    }
+    push (segment) {
+        let result = [];
+        let parts = this.matcher.push(segment);
+        while (parts.length > 0) {
+            const p = parts.shift();
+            if (!p[1]) {
+                result.push([p[0], this.mood])
+            } else {
+                this.mood = !this.mood;
+                if (this.mood) {
+                    this.matcher = this.end_matcher;
+                } else {
+                    this.matcher = this.begin_matcher;
+                }
+                var rest = [];
+                for (const p of parts) {
+                    rest.push(p[0]);
+                }
+                parts = this.matcher.push(rest.join(''));
+            }
+        }
+        return result
+    }
+}
 /*
 * Serial driver *******************************************************
 */
@@ -69,48 +137,30 @@ async function clickConnect() {
     }
 }
 
-var serial_text_buffer = "";
+var line_ending_matcher = new Matcher('\r\n');
+var title_brancher = new Brancher('\x1B]0;', '\x1B\\');
+var last_title_branch = false;
 async function readLoop() {
     // Reads data from the input stream and displays it in the console.
     while (true) {
         const { value, done } = await reader.read();
-        if (value) {
-            if (value.search('\x1B') != -1 || serial_text_buffer.search('\x1B') != -1) {
-                serial_text_buffer += value;
-                if (value.endsWith('\x1B')) {
-                    console.log('broken line ending at ESC');
-                } else if (serial_text_buffer.split('\x1B]').length == serial_text_buffer.split('\x1B\\').length) {
-                    const start_ind = serial_text_buffer.lastIndexOf('\x1B]')
-                    const end_ind = serial_text_buffer.lastIndexOf('\x1B\\')
-                    if (start_ind > end_ind) {
-                        // un reasonable satuation, print and continue
-                        serial.session.insert({row: 1000000, col: 1000000}, serial_text_buffer);
-                        serial_text_buffer = '';
-                    } else {
-                        const by_starts = serial_text_buffer.split('\x1B]0;');
-                        const info = by_starts[by_starts.length-1].split('\x1B\\')[0];
-                        // console.log(info);
-                        document.getElementById('title_bar').innerHTML = info;
-                        serial.session.insert({row: 1000000, col: 1000000}, serial_text_buffer.replace(
-                            /\x1B]0;.*\x1B\\/, ''
-                        ).replace(
-                            /\x1B]0;.*\x1B\\/, ''
-                        ).replace(
-                            /\x1B]0;.*\x1B\\/, ''
-                        )); // replace all has some trouble here
-                        serial_text_buffer = '';
-                    }
+
+        for (branch of title_brancher.push(value)) {
+            title_branch = branch[1];
+            if (title_branch) { // if updating title bar
+                if (last_title_branch) {
+                    document.getElementById('title_bar').innerHTML += branch[0];
+                } else { // if just into this mood
+                    document.getElementById('title_bar').innerHTML = branch[0];
                 }
             } else {
-                if (value.endsWith('\r')) {
-                    serial_text_buffer += value;
-                    // console.log('broken line ending at \\r');
-                } else {
-                    serial.session.insert({row: 1000000, col: 1000000}, serial_text_buffer + value);
-                    serial_text_buffer = '';
+                for (part of line_ending_matcher.push(branch[0])) {
+                    serial.session.insert({row: 1000000, col: 1000000}, part[0]);
                 }
             }
+            last_title_branch = title_branch;
         }
+        
         if (done) {
             console.log('[readLoop] DONE', done);
             reader.releaseLock();
